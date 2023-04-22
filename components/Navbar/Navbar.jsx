223ed73bde91bb85cfb307c0fada3875e820b0a1
Kiran Raj <92741173+KiranRaj27@users.noreply.github.com>
2023-04-22 22:04:39 +0530

    Navbar state added

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -8,9 +8,67 @@ import { CgMenuLeft, CgMenuRight } from "react-icons/cg";
 import { Discover, HelpCenter, Notification, Profile, SideBar } from "./index";
 import { Button } from "../componentindex";
 import Style from "./Navbar.module.css";
+import Images from "../../img";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [discover, setDiscover] = useState(false);
+  const [help, setHelp] = useState(false);
+  const [notification, setNotification] = useState(false);
+  const [profile, setProfile] = useState(false);
+  const [openSideMenu, setOpenSideMenu] = useState(false);
+
+  const openMenu = (e) => {
+    const btnText = e.target.innerText;
+
+    if (btnText == "Discover") {
+      setDiscover(true);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    } else if (btnText == "Help Center") {
+      setDiscover(false);
+      setHelp(true);
+      setNotification(false);
+      setProfile(false);
+    } else {
+      setDiscover(false);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    }
+  };
+
+  return (
+    <div className={Style.navbar}>
+      <div className={Style.navbar_container}>
+        <div className={Style.navbar_container_left}>
+          <div className={Style.log}>
+            <Image
+              src={Images.logo}
+              alt="NFT MARKETPLACE"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className={Style.navbar_container_left_box_input}>
+            <div className={Style.navbar_container_left_box_input_box}>
+              <input type="text" placeholder="Search NFT" />
+              <BsSearch onClick={() => {}} className={Style.search_icon} />
+            </div>
+          </div>
+        </div>
+
+        <div className={Style.navbar_container_right}>
+          <div className={Style.navbar_container_right_discover}>
+            <p onClick={(e) => {}}>Discover</p>
+            <div className={Style.navbar_container_right_discover_box}>
+              <Discover />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
